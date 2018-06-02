02647b9b3888f86f7b12b19a54472e3abb37b11b
sujithra <sujithrakumar13@gmail.com>
2018-06-02 13:23:12 +0530

    uncommitted files

---
+++ WebApplication2/app/directive.js
@@ -0,0 +1,10 @@
+﻿app.directive('searchList', function () {
+	return {
+		restrict: "E",
+		scope: false,
+		templateUrl: 'app/search/search.html',
+		link: function ($scope, element, attrs, toaster) {
+
+		}
+	};
+});
